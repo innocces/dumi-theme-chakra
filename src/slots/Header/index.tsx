1d83bb2a264e24acafea8313e3f50be6a845c005
innocces <18435105624@163.com>
2022-12-07 22:26:59 +0800

    style(header): change header to sticky

---
+++ src/slots/Header/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import {
+  Box,
+  Container,
+  Stack,
+  Show,
+  useColorModeValue
+} from '@chakra-ui/react';
+import ThemeSwitch from '../ThemeSwitch';
+import Social from '../../components/Social';
+import LangSwitch from '../LangSwitch';
+import Logo from '../Logo';
+import SearchBar from '../SearchBar';
+
+const Header = () => {
+  const bg = useColorModeValue('whiteAlpha.800', 'gray.800');
+
+  return (
+    <Box
+      as="header"
+      position="sticky"
+      h={18}
+      top={0}
+      zIndex={10}
+      w="full"
+      bg={bg}
+      backdropFilter="saturate(50%) blur(8px)"
+    >
+      <Container
+        maxW="container.xxl"
+        h="full"
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Logo />
+        <Stack direction="row" spacing={{ base: 0, md: 2.5 }}>
+          <SearchBar />
+          <Show above="md">
+            <Social />
+          </Show>
+          <ThemeSwitch />
+          <LangSwitch />
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+export default Header;
