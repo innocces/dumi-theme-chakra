c97afc0d386e6b9cf259030f8e085fd9653de75f
innocces <18435105624@163.com>
2023-02-28 17:50:48 +0800

    feat(api): asyn dumi multi type api

--- example/src/Button/index.tsx
+++ example/src/Button/index.tsx
@@ -7,6 +7,16 @@ export type ButtonProps = {
    * @description.zh-CN 按钮展示类型
    */
   variant: 'ghost' | 'solid';
+  /**
+   * @description component style
+   * @description.zh-CN 组件样式
+   */
+  style?: React.CSSProperties;
+  /**
+   * @description button click event
+   * @description.zh-CN 点击事件
+   */
+  onClick?: (event: Event) => void;
 };
 
 const Button: FC<PropsWithChildren<ButtonProps>> = ({ children, variant }) => {
--- src/builtins/API/index.tsx
+++ src/builtins/API/index.tsx
@@ -1,5 +1,18 @@
-import React, { useCallback, useMemo, type FC, type ReactNode } from 'react';
+import React, {
+  useCallback,
+  useMemo,
+  useState,
+  useEffect,
+  type FC,
+  type ReactNode,
+  type PropsWithChildren
+} from 'react';
+import type { AtomComponentAsset } from 'dumi-assets-types';
 import { FormattedMessage, useAtomAssets, useRouteMeta, useLocale } from 'dumi';
+
+type PropertySchema = NonNullable<
+  AtomComponentAsset['propsConfig']['properties']
+>[string];
 
 import {
   Tr,
@@ -22,7 +35,120 @@ export type APIProps = {
   default?: ReactNode;
 };
 
-const Code = chakra.code;
+const Code: FC<PropsWithChildren> = ({ children }) => {
+  const codeBgColor = useColorModeValue('gray.50', 'gray.800');
+  const codeColor = useColorModeValue('brand.500', 'brand.300');
+  return (
+    <chakra.code bgColor={codeBgColor} textColor={codeColor}>
+      {children}
+    </chakra.code>
+  );
+};
+
+const HANDLERS = {
+  // entry method
+  toString(prop: PropertySchema): string {
+    if (typeof prop.type === 'string' && prop.type in this) {
+      // value from TypeMap
+      if ('enum' in prop) return this.enum(prop);
+
+      return (this as any)[prop.type](prop);
+    } else if (prop.type) {
+      // non-parsed type, such as ReactNode
+      return this.getValidClassName(prop) || prop.type;
+    } else if ('const' in prop) {
+      // const value
+      return prop.const;
+    } else if ('oneOf' in prop) {
+      // oneOf value
+      return this.oneOf(prop);
+    }
+
+    // unknown type
+    return `unknown`;
+  },
+
+  // type handlers
+  string(prop: PropertySchema) {
+    return prop.type;
+  },
+  number(prop: PropertySchema) {
+    return prop.type;
+  },
+  boolean(prop: PropertySchema) {
+    return prop.type;
+  },
+  any(prop: PropertySchema) {
+    return prop.type;
+  },
+  object(prop: Extract<PropertySchema, { type: 'object' }>) {
+    let props: string[] = [];
+
+    Object.entries(prop.properties || {}).forEach(([key, value]) => {
+      // skip nested object type
+      props.push(
+        `${key}${prop.required?.includes(key) ? '' : '?'}: ${
+          // @ts-ignore
+          value.type === 'object' ? 'object' : this.toString(value)
+        }`
+      );
+    });
+
+    return props.length ? `{ ${props.join('; ')} }` : '{}';
+  },
+  array(prop: Extract<PropertySchema, { type: 'array' }>) {
+    if (prop.items) {
+      const className = this.getValidClassName(prop.items);
+
+      return className ? `${className}[]` : `${this.toString(prop.items)}[]`;
+    }
+
+    return 'any[]';
+  },
+  // FIXME: extract real type
+  element(prop: any) {
+    return `<${prop.componentName} />`;
+  },
+  // FIXME: extract real type
+  function({ signature }: any) {
+    return `${signature.isAsync ? 'async ' : ''}(${signature.arguments
+      .map((arg: any) => `${arg.key}: ${this.toString(arg)}`)
+      .join(', ')}) => ${this.toString(signature.returnType)}`;
+  },
+  // FIXME: extract real type
+  dom(prop: any) {
+    return prop.className || 'DOM';
+  },
+
+  // special handlers
+  enum(prop: PropertySchema) {
+    return prop.enum!.map((v: any) => JSON.stringify(v)).join(' | ');
+  },
+  oneOf(prop: PropertySchema): string {
+    return prop
+      .oneOf!.map((v: any) => this.getValidClassName(v) || this.toString(v))
+      .join(' | ');
+  },
+
+  // utils
+  getValidClassName(prop: PropertySchema) {
+    return 'className' in prop &&
+      typeof prop.className === 'string' &&
+      prop.className !== '__type'
+      ? prop.className
+      : null;
+  }
+};
+
+const APIType: FC<PropertySchema> = (prop) => {
+  const [type, setType] = useState(() => HANDLERS.toString(prop));
+
+  useEffect(() => {
+    setType(HANDLERS.toString(prop));
+  }, [prop]);
+
+  return <Code>{type}</Code>;
+};
 
 const API: FC<APIProps> = (props) => {
   const { frontmatter } = useRouteMeta();
@@ -30,8 +156,6 @@ const API: FC<APIProps> = (props) => {
   const locale = useLocale();
 
   const id = props.id ?? frontmatter.atomId;
-  const codeBgColor = useColorModeValue('gray.50', 'gray.800');
-  const codeColor = useColorModeValue('brand.500', 'brand.300');
 
   if (!id) throw new Error('`id` properties if required for API component!');
 
@@ -108,16 +232,12 @@ const API: FC<APIProps> = (props) => {
                 </Td>
                 <Td>
                   <Skeleton isLoaded={!loading} fadeDuration={4}>
-                    <Code bgColor={codeBgColor} textColor={codeColor}>
-                      {prop.type!}
-                    </Code>
+                    <APIType {...prop} />
                   </Skeleton>
                 </Td>
                 <Td>
                   <Skeleton isLoaded={!loading} fadeDuration={4}>
-                    <Code bgColor={codeBgColor} textColor={codeColor}>
-                      {isReqiured(name, prop.default)}
-                    </Code>
+                    <Code>{isReqiured(name, prop.default)}</Code>
                   </Skeleton>
                 </Td>
               </Tr>
